aedb7401d365468d4363d8fde4751ac655e6cdd4
Cinthya Herrera <herrera@herrera.iram.fr>
2019-09-25 11:56:07 +0200

    flags folder

---
+++ galaxy-specific-scripts/flags-folder/NGC4535-flagfile.py
@@ -0,0 +1,13 @@
+sdflag(infile = 'uid___A002_Xb1d975_X2260.ms.PM02.asap',
+  mode = 'manual',
+  scan = '11,12',
+  overwrite = True)
+sdflag(infile = 'uid___A002_Xb1d975_X2260.ms.PM03.asap',
+  mode = 'manual',
+  scan = '11,12',
+  overwrite = True)
+sdflag(infile = 'uid___A002_Xb1d975_X2260.ms.PM04.asap',
+  mode = 'manual',
+  scan = '11,12',
+  overwrite = True)
+
