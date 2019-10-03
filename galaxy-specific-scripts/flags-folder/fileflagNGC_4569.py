90b3dda5d0911f92e194ec6d993c5e75b8146801
cfaesi <cfaesi@users.noreply.github.com>
2019-10-03 02:14:50 +0200

    Delete fileflagNGC_4569.py

--- galaxy-specific-scripts/flags-folder/fileflagNGC_4569.py
+++
@@ -1,9 +0,0 @@
-sdflag(infile = 'uid___A002_Xd2b681_X13107.ms.PM03.asap',
-  mode = 'manual',
-  scan = '11',
-  overwrite = True)
-sdflag(infile = 'uid___A002_Xd2b681_X13107.ms.PM04.asap',
-  mode = 'manual',
-  scan = '11',
-  overwrite = True)
-
